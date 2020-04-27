64d4e22186648a38ebf3ff35946af8924ea6a3f9
mishakater <51138707+mishakater@users.noreply.github.com>
2020-04-27 15:22:07 +0300

    Initial commit

---
+++ auth/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+//Import Routes
+const authRoute = require('./routes/auth');
+
+dotenv.config();
+
+
+//connect to BD
+mongoose.connect('mongodb+srv://Misha:08i07i@auth-ovogf.gcp.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }, () => console.log('Connected to DB!'));
+
+//Middleware
+app.use(express.json());
+
+
+
+//Route Middlewares
+app.use('/api/user', authRoute);
+
+app.listen(3000, () => console.log('Server up and running'));
+
