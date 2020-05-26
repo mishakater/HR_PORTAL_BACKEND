ccc798d7bce982d9d4bc47eae58206c3fa7efd41
Pavlo <pashapshenishnyuk@gmail.com>
2020-05-26 15:17:33 +0300

    lastName fixed

--- auth/model/User.js
+++ auth/model/User.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema({
         min: 2,
         max: 255
     },
-    surname: {
+    lastName: {
         type: String,
         required: false,
         max: 255
